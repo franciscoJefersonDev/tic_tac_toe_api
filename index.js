7fa69e0ffca7d539b0fc7179d6d1103518270b21
Francisco <francisco.jeferson.dev@gmail.com>
2022-12-04 21:17:48 -0300

    04/12/2022-21:17

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express')
 const http = require('http')
 const { Server } = require('socket.io')
 const cors = require('cors')
-// const ServerlessHttp = require('serverless-http')
 const router = require('./routes')
 require('./bd')
 require('./room_mananger')
@@ -13,12 +12,11 @@ const app = express()
 const server = http.createServer(app)
 app.use(cors())
 app.use(express.json())
-// app.use(router)
-app.use('/', router)
+app.use(router)
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://localhost:5173',
     methods: ['GET', 'POST']
   }
 })
@@ -29,5 +27,4 @@ io.on('connection', async (socket) => {
   // })
 })
 
-// module.exports.handler = ServerlessHttp(app)
 server.listen(process.env.PORT || '3333')
