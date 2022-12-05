0513efc5b60f799bc2e8119b6f434c2f08ac48aa
Francisco <francisco.jeferson.dev@gmail.com>
2022-12-05 10:55:21 -0300

    05/12/2022-10:55

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ app.use(router)
 const io = new Server(server, {
   cors: {
     origin: "*",
-    methods: ['GET', 'POST']
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
   }
 })
 
